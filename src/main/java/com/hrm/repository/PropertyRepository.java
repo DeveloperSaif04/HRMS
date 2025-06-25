24dad6c18a5debeb116793045206b408b1f8f8d3
DevelperSaif04 <saif.coder04@gmail.com>
2025-06-25 15:08:59 +0530

    pagenation sorting

--- src/main/java/com/hrm/repository/PropertyRepository.java
+++ src/main/java/com/hrm/repository/PropertyRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface PropertyRepository extends JpaRepository<Property,Long>, JpaSpecificationExecutor<Property> {
 
+
 }
